d3cb075b9868abac094da48f02b258afc47cd9c0
Guisela-ai19 <guisellamonroy123@gmail.com>
2025-08-19 21:08:57 -0600

    menu principal

---
+++ main.py
@@ -0,0 +1,22 @@
+def MenuPrincipal():
+    print("------- Optimizador de sistemas de agricultura de precisión ---------")
+    print("| Por favor, seleccione una opción:                                  |")
+    print("| 1. Cargar archivo                                                  |")
+    print("| 2. Procesar archivo                                                |")
+    print("| 3. Escribir archivo de salida                                      |")
+    print("| 4. Mostrar datos del estudiante                                    |")
+    print("| 5. Generar grafica                                                 |")
+    print("| 6. Salir                                                           |")
+    print("---------------------------------------------------------------------")
+    opcion = input("Ingrese su opción: ")
+    return opcion
+
+def DatosEstudiante():
+    print("----------------- Datos del Estudiante ------------------")
+    print("| Nombre: Guisela Mishell Monroy Ovalle                   |")
+    print("| Carnet: 202404409                                       |")
+    print("| Introduccion a la programacion y computacion 2          |")
+    print("| Seccion C                                               |")
+    print("| 4to semestre                                            |")
+    print("<<< Enlace a documentacion:                            >>>")
+    print("----------------------------------------------------------")
